172e0599174d36cd2ce078b2ed9472c2a60ccb61
Hack X <81362734+HackX07164@users.noreply.github.com>
2021-10-14 17:41:13 +0200

    Create code-S.py

---
+++ code-S.py
@@ -0,0 +1,27 @@
+import os,sys,time
+############style####################
+# Set Colors ######
+R = '\033[1;31m'
+##################
+os.system ("clear")
+print (R+"""
+         .-""-.
+        / .--. \\
+       / /    \ \\
+       | |    | |
+       | |.-""-.|
+       ///`.::::.`\
+       
+       |||::/  \::;
+       ||;::\__/::;
+       \ \.::::::/
+        `":-..-"`
+        
+""")
+
+H1 = input (R+'❯'+Y+'❯'+G+'❯ '+C)
+time.sleep(1)
+print (R+' R '+Y+'u '+G+'n '+B+'n '+N+'i '+C+'n '+G+'g .'+R+'.'+Y+'.')
+time.sleep(3)
+if H1 == "HX-WE code S":
+        os.system ("python HX-WE.py")
